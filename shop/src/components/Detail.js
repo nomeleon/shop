cbb6d8bdff2d79914403953ee0e0b61f6601aa63
노소연 <yeonie@kakao.com>
2022-08-03 17:34:10 +0900

    tab ui

--- shop/src/components/Detail.js
+++ shop/src/components/Detail.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Nav } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 // import styled from "styled-components";
 
@@ -18,6 +19,7 @@ const Detail = (props) => {
   });
 
   let [alert, setAlert] = useState(true);
+  let [tab, setTab] = useState(0);
 
   //component mount, update 시 코드 실행 : html 렌더링 후에 동작한다.
   //어려운 연산, 서버에서 데이터 가져오는 작업, 타이머 장착 등
@@ -50,8 +52,53 @@ const Detail = (props) => {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link0"
+            onClick={() => {
+              setTab(0);
+            }}
+          >
+            버튼0
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link1"
+            onClick={() => {
+              setTab(1);
+            }}
+          >
+            버튼1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link2"
+            onClick={() => {
+              setTab(2);
+            }}
+          >
+            버튼2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent tab={tab} />
     </div>
   );
 };
+
+function TabContent({ tab }) {
+  // if (tab == 0) {
+  //   return <div>내용0</div>;
+  // } else if (tab == 1) {
+  //   return <div>내용1</div>;
+  // } else if (tab == 2) {
+  //   return <div>내용2</div>;
+  // }
+  return [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab];
+}
 
 export default Detail;
